40e142c2ba57aafc74bbe9d1f0b68f19bb7a7f65
Gasprinskiy <gasprin321@gmail.com>
2023-08-10 23:56:24 +0500

    feat(auth-view): auth view init

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import AuthWiew from "@/views/AuthWiew.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  routes: [
+    {
+      path: "/",
+      name: "Auth",
+      component: AuthWiew
+    },
+  ]
 })
 
 export default router
---
+++ src/views/AuthWiew.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="auth-view full-screen">
+      <n-card>
+        <n-form>
+          <n-form-item
+            label="Эл. почта"
+          >
+            <n-input
+              type="text"
+              v-model:value="email"
+            />
+          </n-form-item>
+          <n-form-item
+              label="Пароль"
+          >
+            <n-input
+              type="password"
+              v-model:value="password"
+            />
+          </n-form-item>
+        </n-form>
+      </n-card>
+    </div>
+</template>
+  
+<script setup lang="ts">
+import { NCard, NForm, NFormItem, NInput } from "naive-ui";
+import { ref } from "vue";
+
+const email = ref<string|null>(null)
+const password = ref<string|null>(null)
+
+</script>
+  
+<style scoped>
+  .auth-view {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .n-card {
+      max-width: 500px;
+    }
+  }
+</style>
+  
