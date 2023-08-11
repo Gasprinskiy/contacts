534ad4770f1346b0fbb77b829f600c7dd75f07e3
samgasper <zakirov.ismail@sarkor.uz>
2023-08-11 17:48:19 +0500

    feat(app-vue): route guard

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,36 @@
 <template>
-    <RouterView/>
+   <router-view/>
 </template>
 
 <script setup lang="ts">
-import { RouterView } from "vue-router";
+import { RouterView, useRoute, useRouter } from "vue-router";
+import { useNotification } from "naive-ui";
+import { computed, inject, onBeforeMount } from "vue";
+import { AuthInjectionKey } from "@/request_worker/";
+import { useAppRequestHandler } from "@/composables/app_req_handler/";
+import app_bus from "./shared/app_bus";
+
+const router = useRouter()
+const route = useRoute()
+const notification = useNotification()
+const authRequestWorker = inject(AuthInjectionKey)!
+
+const notAuthRoute = computed(() => route.name !== "Auth")
+
+app_bus.on("unauthorized_request", () => {
+    if(notAuthRoute.value) {
+        notification.warning({
+            title: "Необходимо авторизоватся в системе",
+            duration: 3000
+        })
+        router.push("/auth")
+    }
+})
+
+const handleCheckAuth = useAppRequestHandler(authRequestWorker.checkAuth)
+
+onBeforeMount(async () => await handleCheckAuth())
+
 </script>
 
 <style scoped>
---
+++ src/AppWrapper.vue
@@ -0,0 +1,10 @@
+<template>
+    <n-notification-provider>
+        <App/>
+    </n-notification-provider>
+</template>
+
+<script setup lang="ts">
+import { NNotificationProvider } from "naive-ui";
+import App from "./App.vue";
+</script>
