2a9c7d4159996190b0b8d39a5b0a2d5573ff7528
Gasprinskiy <gasprin321@gmail.com>
2023-08-11 23:57:01 +0500

    style(request-worker): injection keys create style

--- src/request_worker/index.ts
+++ src/request_worker/index.ts
@@ -1,14 +1,14 @@
 import type { InjectionKey } from 'vue'
-import type { Auth } from "./core/auth";
-import type { Contacts } from "./core/contacts";
+import type { Auth } from "./core/auth/";
+import type { Contacts } from "./core/contacts/";
 import { AuthImpl } from "./core/auth";
 import { ContactsImpl } from "./core/contacts";
 
 type InjectionKeys = {[key: string]: InjectionKey<any>}
 type InjectionImpl = {[key: string]: object}
 
-export const AuthInjectionKey = Symbol() as InjectionKey<Auth>
-export const ContactsInjectionKey = Symbol() as InjectionKey<Contacts>
+export const AuthInjectionKey : InjectionKey<Auth> = Symbol("Auth")
+export const ContactsInjectionKey: InjectionKey<Contacts> = Symbol("ContactsInjectionKey")
 
 export const InjectionKeysMap: InjectionKeys = {
     auth: AuthInjectionKey,
