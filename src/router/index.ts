a055617e15c2b06d613c3acda45a4d9486e0335d
samgasper <zakirov.ismail@sarkor.uz>
2023-08-11 17:52:13 +0500

    feat(views): HomeView init

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import AuthWiew from "@/views/AuthWiew.vue";
+import HomeView from "@/views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
+      name: "Home",
+      component: HomeView
+    },
+    {
+      path: "/auth",
       name: "Auth",
       component: AuthWiew
     },
