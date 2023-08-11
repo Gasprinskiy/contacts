9cd2e77fee12f6b23244428fa194067739b2a637
Gasprinskiy <gasprin321@gmail.com>
2023-08-12 00:01:35 +0500

    style(request_wroker): removed cabel case in app_bus import

--- src/request_worker/handler/index.ts
+++ src/request_worker/handler/index.ts
@@ -1,4 +1,4 @@
-import app_bus from "@/shared/app_bus"
+import appBus from "@/shared/app_bus"
 
 // lat's pretend that is axios instance
 export const handleRepoRequest = async <T>(request: (...args: any[]) => Promise<T>, ...args: any[]) : Promise<T|any> => {
@@ -6,7 +6,7 @@ export const handleRepoRequest = async <T>(request: (...args: any[]) => Promise<
         return await request(...args)
     } catch(err: any) {
         if (err.code === "ErrNotAuthorized") {
-            app_bus.emit("unauthorized_request")
+            appBus.emit("unauthorized_request")
             return 
         }
         throw err
