dccdf5a3fc3445c50c5ca757a376491635cf48df
Gasprinskiy <gasprin321@gmail.com>
2023-08-12 13:37:56 +0500

    style(main.ts): changed import style; changed injection key init.

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
-import './assets/main.css'
-import './shim.d.ts'
-import { createApp } from 'vue'
-import App from './AppWrapper.vue'
-import router from './router'
-import { InjectionKeysMap, InjectionImplMap } from './request_worker'
+import "./assets/main.css";
+import "./shim.d.ts";
+import { createApp } from "vue";
+import App from "./AppWrapper.vue";
+import router from "./router";
+import { InjectionKeysMap, InjectionImplMap } from "./request_worker";
 
 const app = createApp(App)
 
-Object.keys(InjectionKeysMap).forEach(injection => {
-    app.provide(InjectionKeysMap[injection], InjectionImplMap[injection])
+Object.keys(InjectionKeysMap).forEach(key => {
+    app.provide(InjectionKeysMap[key], InjectionImplMap[key])
 })
 
 app.use(router)
