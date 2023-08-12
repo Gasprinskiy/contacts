d0c6f886138165b03b14951b3dc658bc89e6fb20
Gasprinskiy <gasprin321@gmail.com>
2023-08-12 13:48:45 +0500

    feat(home-view): add reset values method; add logOutMethod and button which trigers it;

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,14 +5,28 @@
           <h1>
             Контакты
           </h1>
-          <n-button>
-            <template #icon>
-              <n-icon>
-                <AddCircleOutline/>
-              </n-icon>
-            </template>
-            Добавить контакт
-          </n-button>
+          <div class="header-buttons">
+            <n-button
+              @click="goToContactCreate"
+              type="primary"
+            >
+              <template #icon>
+                <n-icon>
+                  <AddCircleOutline/>
+                </n-icon>
+              </template>
+              Добавить контакт
+            </n-button>
+            <n-button
+              @click="signOut"
+            >
+              <template #icon>
+                <n-icon>
+                  <LogOutOutline/>
+                </n-icon>
+              </template>
+            </n-button>
+          </div>
         </n-layout-header>
         <n-layout-content class="search-bar">
           <n-form
@@ -47,7 +61,7 @@
                 secondary 
                 type="warning"
                 attr-type="reset"
-                @click="loadContacts"
+                @click="resetValues"
               >
                 Сбросить
               </n-button>
@@ -87,16 +101,18 @@ import {
   NDataTable,
   NTag
 } from "naive-ui";
-import { AddCircleOutline } from "@vicons/ionicons5";
+import { AddCircleOutline, LogOutOutline } from "@vicons/ionicons5";
 import { h, inject, onBeforeMount, ref} from "vue";
 import { useRouter } from "vue-router";
-import { ContactsInjectionKey } from "@/request_worker";
+import { ContactsInjectionKey, AuthInjectionKey } from "@/request_worker";
 import { useAppRequestHandler } from "@/composables/app_req_handler";
 import type { Contact, ContactTags} from "@/request_worker/entity/contact/entity";
 import type { ContactSearchParam } from "@/request_worker/entity/contact/params";
 
 const router = useRouter()
 const contactRequestWorker = inject(ContactsInjectionKey)!
+const authRequestWorker = inject(AuthInjectionKey)!
+
 const searchValues = ref<ContactSearchParam>({
   query: "",
 })
@@ -107,7 +123,6 @@ const validationRules : FormRules = {
     message: "введите запрос"
   }
 }
-
 const tagsList = ref<ContactTags[] | null>(null)
 const contactList = ref<Contact[] | null>(null)
 const contactListColumns = ref<DataTableColumns<Contact> | null>(null)
@@ -115,8 +130,9 @@ const contactListColumns = ref<DataTableColumns<Contact> | null>(null)
 const handleLoadTags = useAppRequestHandler(contactRequestWorker.loadTags)
 const handleLoadContacts = useAppRequestHandler(contactRequestWorker.loadContactsList)
 const handleFinContractRequest = useAppRequestHandler(contactRequestWorker.findContact)
+const handleSignOut = useAppRequestHandler(authRequestWorker.signOut)
 
-const createContactColumns = (goToContact: (rowData: Contact) => void) : DataTableColumns<Contact> => {
+const createContactColumns = (goToContact: (row: Contact) => void) : DataTableColumns<Contact> => {
   return [
     {
       title: "ФИО",
@@ -134,7 +150,7 @@ const createContactColumns = (goToContact: (rowData: Contact) => void) : DataTab
       title: "Теги",
       key: "tags",
       render: (row: Contact) => {
-        return row.tags.map((tag: string) => {
+        return row.tags?.map((tag: ContactTags) => {
           return h(
               NTag,
               {
@@ -144,7 +160,7 @@ const createContactColumns = (goToContact: (rowData: Contact) => void) : DataTab
                 type: 'info',
                 bordered: false
               },
-              {default: () => tag}
+              {default: () => tag.value}
           )
         })
       }
@@ -169,18 +185,20 @@ const goToContact = (row: Contact) : void => {
   router.push(`/contact/${row.id}`)
 }
 
-const loadTags = async () => {
-  tagsList.value = await handleLoadTags()
-  console.log(tagsList.value);
-  
+const goToContactCreate = () : void => {
+  router.push(`/contact_create`)
 }
 
-const loadContacts = async () => {
+const loadTags = async () : Promise<void> => {
+  tagsList.value = await handleLoadTags()
+}
+
+const loadContacts = async () : Promise<void> => {
   contactList.value = await handleLoadContacts()
   contactListColumns.value = createContactColumns(goToContact)
 }
 
-const findContract = async (): Promise<void> => {
+const findContract = async () : Promise<void> => {
   searchFormRef.value?.validate(async (err) => {
     if (err) {
       return
@@ -188,6 +206,19 @@ const findContract = async (): Promise<void> => {
 
     contactList.value = await handleFinContractRequest(searchValues.value)
   })
+}
+
+const resetValues = async () : Promise<void> => {
+  searchValues.value.query = ""
+  searchValues.value.tagIdList = []
+
+  await loadContacts()
+}
+
+const signOut = async () : Promise<void> => {
+  await handleSignOut()
+
+  router.push("/auth")
 }
 
 onBeforeMount(async () => {
@@ -212,6 +243,11 @@ onBeforeMount(async () => {
 
   .contact-table {
     margin-top: 20px;
+  }
+
+  .header-buttons {
+    display: flex;
+    gap: 5px;
   }
 
 </style>
